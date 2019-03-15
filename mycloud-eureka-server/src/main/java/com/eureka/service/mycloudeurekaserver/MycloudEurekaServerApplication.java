d3dddc1f5d6ccaadaaa4a32184dcc9c776b2ea81
zf <820674670@qq.com>
2019-03-15 11:22:09 +0800

    eureka服务注册中心

--- mycloud-eureka-server/src/main/java/com/eureka/service/mycloudeurekaserver/MycloudEurekaServerApplication.java
+++ mycloud-eureka-server/src/main/java/com/eureka/service/mycloudeurekaserver/MycloudEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.eureka.service.mycloudeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MycloudEurekaServerApplication {
 
 	public static void main(String[] args) {
