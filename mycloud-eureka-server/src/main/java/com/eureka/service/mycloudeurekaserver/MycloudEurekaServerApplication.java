081b1b8278668066b972e2bf6ffbb3bc10d777c7
zf <820674670@qq.com>
2019-03-15 09:31:17 +0800

    eureka服务注册中心

---
+++ mycloud-eureka-server/src/main/java/com/eureka/service/mycloudeurekaserver/MycloudEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.service.mycloudeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycloudEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycloudEurekaServerApplication.class, args);
+	}
+
+}
